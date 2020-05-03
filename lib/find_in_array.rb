c6e53eed0c02cbfd9790a9628a8c916dfea52451
Spencer Smith <smithspencer817@gmail.com>
2020-05-03 19:42:56 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,9 @@
 def find_element_index(array, value_to_find)
   count = 0
-  value_index = nil
   while count < array.length do
     if array[count] == value_to_find
-      value_index = count
-      value_index
-    count += 1 
+      return count
+    end
+    count += 1
   end
-  value_index
 end
